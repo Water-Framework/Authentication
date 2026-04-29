2c42645174e98b1cc8fb1596dd4be388bf1e4233
Aristide <aristide.cittadino@acsoftware.it>
2026-04-29 08:49:56 +0200

    adding properties for token generation

--- Authentication-service/src/main/java/it/water/authentication/service/AuthenticationConstants.java
+++ Authentication-service/src/main/java/it/water/authentication/service/AuthenticationConstants.java
@@ -6,4 +6,9 @@ public class AuthenticationConstants {
     }
 
     public static final String AUTHENTICATION_ISSUER_NAME = "water.authentication.service.issuer";
+
+    public static final String KEYSTORE_PASSWORD = "water.keystore.password";
+    public static final String KEYSTORE_ALIAS = "water.keystore.alias";
+    public static final String KEYSTORE_FILE = "water.keystore.file";
+    public static final String PRIVATE_KEY_PASSWORD = "water.private.key.password";
 }
